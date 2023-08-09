6ece68a70a492d909bd66863d0f08333acbf90a8
jecosine <jecosine@gmail.com>
2023-08-10 05:58:25 +1000

    feat: implement preprocess of 'iris' dataset

--- allib/datasets/avail_datasets.py
+++ allib/datasets/avail_datasets.py
@@ -1,6 +1,9 @@
 import json
+import logging
 import os.path
 import pickle
+from importlib import resources
+from typing import Optional, Tuple
 
 import pandas as pd
 
@@ -8,14 +11,15 @@ from allib.constants import *
 from allib.utils import ensure_path, validate_dataset
 from .tools import _download_dataset, _test_download
 
+
 URL_TEMPLATE = "https://archive.ics.uci.edu/static/public/%s/%s.zip"
 UCI_DB = None
 
 
-# print(os.getcwd(), os.path.abspath(os.path.curdir))
+__logger = logging.Logger(__name__)
 
 
-def _load_raw_uci(raw_path: str = "./uci_db.json"):
+def _load_raw_uci(raw_path: str = "uci_db.json"):
     global UCI_DB
     db_cache_path = os.path.join(CACHE_DIR, "meta")
     ensure_path(db_cache_path)
@@ -23,8 +27,10 @@ def _load_raw_uci(raw_path: str = "./uci_db.json"):
     # process meta data
     if UCI_DB is None:
         if not ensure_path(meta_path, is_dir=False):
-            with open(raw_path, "rb") as f:
-                raw_data = json.load(f)
+            # with open(raw_path, "rb") as f:
+            #     raw_data = json.load(f)
+            raw_data = resources.files("allib.datasets").joinpath(raw_path).read_text()
+            raw_data = json.loads(raw_data)
             processed = {}
             for i in raw_data:
                 processed[i["Name"].strip().lower().replace(" ", "-")] = i
@@ -36,9 +42,39 @@ def _load_raw_uci(raw_path: str = "./uci_db.json"):
                 UCI_DB = pickle.load(f)
 
 
+def get_uci_db() -> dict:
+    global UCI_DB
+    _load_raw_uci()
+    return UCI_DB or {}
+
+
+def iris(path: Optional[str] = None) -> Tuple[pd.DataFrame, pd.DataFrame]:
+    if not UCI_DB:
+        _load_raw_uci()
+    path = path or os.path.join(CACHE_DIR, "iris")
+    checklists = [os.path.join(path, i) for i in ["iris.data", "bezdekIris.data"]]
+    for p in checklists:
+        ensure_path(p, is_dir=False)
+    attrs = UCI_DB["iris"]["attributes"]
+    columns = []
+    for attr in attrs:
+        columns.append(attr["name"].strip().lower().replace(" ", "_"))
+    d1 = pd.read_csv(checklists[0], skiprows=0, names=columns)
+    d2 = pd.read_csv(checklists[1], skiprows=0, names=columns)
+    data = pd.concat((d1, d2)).reset_index(drop=True)
+    # separate label column
+    data = data.rename(columns={"class": "label"})
+    label = data.label
+    data = data.drop(columns=["label"])
+    return data, label
+
+
+AVAIL_DATASET = {"iris": iris}
+
+
 def load_uci(
-    name: str, reload: bool = False, test: bool = False, raw_path: str = "./uci_db.json"
-):
+    name: str, reload: bool = False, test: bool = False, raw_path: str = "uci_db.json"
+) -> Tuple[pd.DataFrame, pd.DataFrame]:
     """ Load datasets from UCI repo by name
 
     Args:
@@ -50,26 +86,26 @@ def load_uci(
     Returns:
         pd.DataFrame: the dataset in pandas.DataFrame form
     """
-    _load_raw_uci(raw_path)
+    if not UCI_DB:
+        _load_raw_uci(raw_path)
     name = name.lower()
     if name not in UCI_DB:
         # todo: new exception
         raise RuntimeError(f"Cannot find {name} in UCI Repo")
     dataset = UCI_DB[name]
     dataset_path = os.path.join(CACHE_DIR, name)
-    if reload or validate_dataset(dataset_path):
+    # still reload if not valid
+    if reload or not validate_dataset(dataset_path):
         url = URL_TEMPLATE % (dataset["ID"], dataset["slug"])
         if not test:
             _download_dataset(url, "", name)
         else:
             _test_download(url, name)
 
-
-def get_uci_db() -> dict:
-    global UCI_DB
-    return UCI_DB or {}
-
-
-def iris(path: str):
-    checklists = [os.path.join(path, i) for i in ["iris.data", "bezdekIris.data"]]
-    data1 = pd.read_csv(checklists[0])
+    if name not in AVAIL_DATASET:
+        __logger.warning(
+            f"Dataset {name} downloaded, but it need to be manually processed"
+        )
+    else:
+        handler = AVAIL_DATASET[name]
+        return handler()
